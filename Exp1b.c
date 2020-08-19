612f7dafe40fab04f785cce1f5411f95b16c2ef7
apoorvlodhi-io <apoorvlodhi21@gmail.com>
2020-08-19 15:45:48 +0530

    Init commit

---
+++ Exp1b.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+void forkexample() 
+{  
+    if (fork() == 0) 
+        printf("Hello from Apoorv!\n"); 
+    else
+        printf("Hello from 181112005!\n"); 
+} 
+int main() 
+{ 
+    forkexample(); 
+    return 0; 
+}
